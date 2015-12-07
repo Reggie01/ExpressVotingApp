d59a4207b79d2da839cdabe0ead54dd3ecb9c458
Reggie S <reggie@bgcreatives.com>
2015-12-06 22:12:26 -0500

    Initial commit

---
+++ site/index.js
@@ -0,0 +1,53 @@
+﻿'use strict';
+
+var express = require("express"),
+              app = express(),
+             port = 3001,
+         voting  = require(__dirname + '/app/voting.js');
+             
+app.set('port', process.env.PORT || port);
+// set up handlebars view engine
+var handlebars = require('express3-handlebars')
+     .create( { defaultLayout: 'main' } );
+app.engine( 'handlebars', handlebars.engine );
+app.set( 'view engine', 'handlebars' );
+           
+// Log Date every time request is made
+app.use(function (req, res, next) {
+ var date = new Date();
+ if(date) {
+   console.log('Time: %d%d%d',  date.getHours(), date.getMinutes(), date.getSeconds()  );
+ } 
+ next();
+});
+
+app.use(express.static(__dirname + "/public"));
+
+app.get("/", function( req, res ) {
+   res.render('home', { name : "Voting" });
+});
+
+app.get("/about", function( req, res ) {
+ console.log(voting);
+   var randomFortune = voting[Math.floor(Math.random() * voting.length)];
+   res.render("about", { fortune: randomFortune })
+});
+
+// custom 404 page
+app.use( function( req, res ) {
+  res.status( 404 );
+  res.render("404")
+});
+
+// custom 500 page
+app.use( function(err, req, res, next ){
+  console.error( err.stack );
+  res.status(500);
+  res.render("500");
+});
+
+
+
+app.listen( app.get('port'), function() {
+  console.log( "Express started on http://localhost: " + app.get( "port" ) + "; Press Ctrl-C to terminate." );
+});
---
+++ site/public/js/voting.js
@@ -0,0 +1,10 @@
+﻿'use strict';
+
+var fortunes = [
+  "Conquer your fears or they will conquer you.",
+  "Rivers need springs",
+  "You will have a pleasant surprise.",
+  "Whenever possible, keep it simple."
+];
+
+console.log(fortunes);
